071554dd4f99496d279985290d0993f583be9a22
Hidde Ottens <hidde95@gmail.com>
2020-06-19 15:30:13 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = function() {
-    
+const fs = require('fs-extra');
+
+function loadFonts() {
+    return new Promise((reject, resolve) => {
+        fs.copy('/node_modules/font-loader/fonts', '/tmp', err => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve('/tmp/fonts');
+            }
+        });
+    })
 }
+
+module.exports = {
+  loadFonts
+};
