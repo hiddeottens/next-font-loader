7d625a549c2e5c4817d3e7fad8d299479f920f9e
Hidde Ottens <hidde95@gmail.com>
2020-06-23 21:30:51 +0200

    Use module path

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const fs = require('node-fs-extra');
 const path = require('path');
 
 function loadFonts() {
+    const modulePath = require.resolve('next-font-loader');
     return new Promise((resolve, reject) => {
-        fs.copy('/node_modules/next-font-loader/fonts', '/tmp', err => {
+        fs.copy(`${modulePath}/fonts`, '/tmp', err => {
             if (err) {
               reject(err);
             } else {
               resolve(path.join(process.cwd(), '/tmp'));
             }
         });
-    })
+    });
 }
 
 module.exports = {
